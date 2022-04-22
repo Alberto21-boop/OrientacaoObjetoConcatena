9b82ab2cf9d4fccfb07149e5a956367d6adf6aa1
alberto <albertobarbosa0003@gmail.com>
2022-04-22 14:26:06 -0300

    Adicionar arquivos de projeto.

---
+++ app11/CaarosSoma.cs
@@ -0,0 +1,8 @@
+﻿using app11;
+
+Carros carros = new Carros();
+
+carros.carro1 = Console.ReadLine();
+carros.carro2 = Console.ReadLine();
+
+Console.WriteLine($"Carros : {carros.DoisCarros()}");
---
+++ app11/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace app11
+{
+    internal class Carros
+    {
+        public string carro1 { get; set; } = default!;
+        public string carro2 { get; set; } = default!;  
+
+        public string DoisCarros()
+        {
+            return carro1 + carro2;
+        }
+    }
+}
